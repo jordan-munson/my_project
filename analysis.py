4512dc7a01acc244551c74e798d2b3c81a128c91
jordan-munson <35117187+jordan-munson@users.noreply.github.com>
2018-03-01 11:10:23 -0800

    Make countries the index

--- analysis.py
+++ analysis.py
@@ -8,9 +8,9 @@ Software Carpentry Workshop
 import pandas as pd
 
 #Read data files
-gdp_europe = pd.read_csv('data/gapminder_gdp_europe.csv')
-gdp_oceania = pd.read_csv('data/gapminder_gdp_oceania.csv')
-gdp_asia = pd.read_csv('data/gapminder_gdp_asia.csv')
-gdp_africa = pd.read_csv('data/gapminder_gdp_africa.csv')
-gdp_all = pd.read_csv('data/gapminder_all.csv')
+gdp_europe = pd.read_csv('data/gapminder_gdp_europe.csv', index_col = 'country')
+gdp_oceania = pd.read_csv('data/gapminder_gdp_oceania.csv', index_col = 'country')
+gdp_asia = pd.read_csv('data/gapminder_gdp_asia.csv', index_col = 'country')
+gdp_africa = pd.read_csv('data/gapminder_gdp_africa.csv', index_col = 'country')
+gdp_all = pd.read_csv('data/gapminder_all.csv', index_col = 'country')
 
