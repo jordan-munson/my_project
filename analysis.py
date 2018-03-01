04b2745016a870e6da59c9c186e91753791d6330
jordan-munson <jordan.munson@wsu.edu>
2018-03-01 10:12:15 -0800

    Created analysis.py

---
+++ analysis.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Software Carpentry Workshop
+"""
+#Import dealios
+import pandas as pd
+
+#Read data files
+dat = pd.read_csv('data/gapminder_gdp_europe.csv')
+
+
