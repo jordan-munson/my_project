c86a252f2add99b52f511835ac5c40a119eaae27
jordan-munson <jordan.munson@wsu.edu>
2018-03-01 10:19:21 -0800

    added reading all gdp data files

--- analysis.py
+++ analysis.py
@@ -9,5 +9,8 @@ import pandas as pd
 
 #Read data files
 gdp_europe = pd.read_csv('data/gapminder_gdp_europe.csv')
-gdp_oceania = pd.read_csv('data/gapminder_gdp_oceania')
+gdp_oceania = pd.read_csv('data/gapminder_gdp_oceania.csv')
+gdp_asia = pd.read_csv('data/gapminder_gdp_asia.csv')
+gdp_africa = pd.read_csv('data/gapminder_gdp_africa.csv')
+gdp_all = pd.read_csv('data/gapminder_all.csv')
 
